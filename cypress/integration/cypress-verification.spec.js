d151715f2ee96920b256f48a47fa91df909ac598
Max Ponomarenko <maxeleron@gmail.com>
2020-12-03 08:45:36 +0200

    added_Cypress_for_TDD

---
+++ cypress/integration/cypress-verification.spec.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+describe('testing how e2e test works', () => {
+    it('actually works', () => {
+        expect(true).to.equal(true);
+    })
+})
---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+          Hello React 😎!
+
+        <nav>
+          <li>Main</li>
+          <li>Fav. exercises</li>
+          <li>Excercises</li>
+          <li>Support</li>
+        </nav>
+      </header>
+      
+      <main>
+        <p>Some text</p>
+      </main>
+
+      <footer>
+        <p>Support the project</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
